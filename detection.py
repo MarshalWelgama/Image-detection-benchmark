ad8e66920edd8a7020e1e95bc772b98cf948a3fe
Marshal Welgama <marshalwelgama@gmail.com>
2021-09-16 17:59:28 +1000

    fix: removed debug code, clean up

--- detection.py
+++ detection.py
@@ -1,12 +1,10 @@
+# Author: Marshal Welgama
 import cvlib as cv
-import sys
 import cv2
-import os 
 import urllib.request
 import numpy as np
 import time
 import _thread
-
 
 num_threads = 0
 def detect_face(threadName):
@@ -15,29 +13,19 @@ def detect_face(threadName):
         num_threads += 1
         start = time.process_time()
         req = urllib.request.urlopen('https://100k-faces.glitch.me/random-image')
+        
         arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        image = cv2.imdecode(arr, -1) # 'Load it as it is'
-        # read input image
-        # image = cv2.imread("https://ozgrozer.github.io/100k-faces/0/0/000749.jpg")
-        # apply face detection
+        image = cv2.imdecode(arr, -1) 
         faces, confidences = cv.detect_face(image)
-        # loop through detected faces
         for face,conf in zip(faces,confidences):
-
             (startX,startY) = int(face[0]),int(face[1])
             (endX,endY) = int(face[2]),int(face[3])
+            cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)    
 
-            # draw rectangle over face
-            cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
-        # display output
-        # press any key to close window   
-        # save output
-        
         cv2.imwrite("{}.jpg".format(threadName), image)
         end = time.process_time()  
         elapsed = end - start
         print(threadName, elapsed, "done")
-        # release resources
         cv2.destroyAllWindows()
         num_threads -= 1
     except:
